4482b544ce42c4bd7ce26b08ef9643b473275a53
imseongtae <dev.haemil@gmail.com>
2021-04-05 01:18:57 +0900

    chore: Project setting

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,5 +2,15 @@ module.exports = {
   extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
-  rules: {},
-}
+  rules: {
+    'at-rule-no-unknown': null,
+    'no-empty-source': null,
+    'rule-empty-line-before': null,
+    'selector-list-comma-newline-after': null,
+    'no-descending-specificity': null,
+    'declaration-block-trailing-semicolon': null,
+    'declaration-empty-line-before': null,
+    // 'at-rule-empty-line-before': 'never',
+    'at-rule-empty-line-before': null,
+  },
+};
