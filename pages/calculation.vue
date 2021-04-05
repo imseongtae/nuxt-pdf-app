3e2c5cb2bac58b14c1aa716f924d1e5c2a624ecd
imseongtae <dev.haemil@gmail.com>
2021-04-06 02:05:17 +0900

    feat: Add calculation page

---
+++ pages/calculation.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="caculation-page">
+    <!--  -->
+    <h2 class="caculation-page__title">Calculation</h2>
+
+    <!-- pdf app -->
+    <client-only>
+      <pdf-viewer
+        class="pdf"
+        :src="pdfSrc"
+        :page="1"
+        @num-pages="totalCount = $event"
+      />
+    </client-only>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Calculation',
+  data() {
+    return {
+      totalCount: 0,
+      pdfSrc: require('@/assets/pdf/sample.pdf'),
+    };
+  },
+};
+</script>
+
+<style scoped>
+.pdf {
+  width: 100%;
+  height: 100%;
+  border: 1px solid red;
+  user-select: none;
+}
+
+.caculation-page__title {
+  color: dodgerblue;
+}
+</style>
